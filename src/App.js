508e0fda476b7c9bd93cdfe95c604f1493dc7562
Your Name <yuvraj.garg101@gmail.com>
2022-06-02 14:30:54 +0530

    added LOGIC text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { quickSort , quickSort_simple } from './QuickSort'
 import { heapSort , heapSort_simple} from './HeapSort'
 
 var sum_delay = 0
-var delay = 1
+var delay = 0.1
 export function color_update(array__bar,new_color){
   setTimeout(() => {
       array__bar.style.backgroundColor = new_color;
@@ -26,7 +26,6 @@ function App() {
   useEffect(()=>{
     generateNewArray()
   },[])
-  var logic_div = document.getElementsByClassName("app__algo__logic")
   const getRandom = (a,b) => {
     return Math.floor(a+(Math.random()*b))
   }
@@ -36,9 +35,13 @@ function App() {
       a.push(getRandom(100,450));
     }
     setArray(a);
-    arrayColorUpdate();
+    updateArrayBarsColor();
+    updateAlgoLogic("");
   }
-  function arrayColorUpdate(){
+  function updateAlgoLogic(s){
+    document.getElementById("app__algo__logic").innerText = s;
+  }
+  function updateArrayBarsColor(){
     const Bars = document.getElementsByClassName("app__array__container__bar")
     for( let i = 0 ; i < array.length ; i++ ){ 
       Bars[i].style.backgroundColor = 'aqua'
@@ -71,75 +74,44 @@ function App() {
     sum_delay = 0
     bubbleSort(array);
     console.log("bubble sort")
-    logic_div.innerHTML = "The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from right to left. Each time we will start from the left most point and compare 2 adjacent elements A(i) and A(i+1) for i = 0 to n-2 and swap them if A(i) > A(i+1). This way the largest element will reach the end and we will continue the process"
-    console.log(logic_div.innerHTML)
+    var s = "LOGIC:  The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from right to left. Each time we will start from the left most point and compare 2 adjacent elements A(i) and A(i+1) for i = 0 to n-2 and swap them if A(i) > A(i+1). This way the largest element will reach the end and we will continue the process"
+    updateAlgoLogic(s);
+    // console.log(logic_div.innerText)
   }
   const InsertionSortClick = () => {
     sum_delay = 0
     insertionSort(array);
     console.log("insertion sort")
-    logic_div.innerHTML = "The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from left to right. Each time we will pick the left most element from the unsorted array and insert it at it's correct position in the unsorted array on it's left. This way the element will be inserted at it's correct position and the whole array will be sorted like this"
+    var s = "LOGIC: The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from left to right. Each time we will pick the left most element from the unsorted array and insert it at it's correct position in the unsorted array on it's left. This way the element will be inserted at it's correct position and the whole array will be sorted like this"
+    updateAlgoLogic(s);
   }
 
   const SelectionSortClick = () => {
     sum_delay = 0
     selectionSort(array);
     console.log("selection sort")
-    logic_div.innerHTML = "The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from left to right. Each time we will find the minimum element of the unsorted array and swap it with the first element of the unsorted array. This way the element will be inserted at it's correct position and the whole array will be sorted like this"
+    var s = "LOGIC: The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from left to right. Each time we will find the minimum element of the unsorted array and swap it with the first element of the unsorted array. This way the element will be inserted at it's correct position and the whole array will be sorted like this"
+    updateAlgoLogic(s);
   }
 
   const MergeSortClick = () => {
-    sum_delay = 0
-    mergeSort(array);
-    logic_div.innerHTML = "The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from left to right. Each time we will find the minimum element of the unsorted array and swap it with the first element of the unsorted array. This way the element will be inserted at it's correct position and the whole array will be sorted like this"
+  //   sum_delay = 0
+  //   mergeSort(array);
+  //   var s = "LOGIC: The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from left to right. Each time we will find the minimum element of the unsorted array and swap it with the first element of the unsorted array. This way the element will be inserted at it's correct position and the whole array will be sorted like this"
+  //   updateAlgoLogic(s);
   }
   const HeapSortClick = () => {
-    sum_delay = 0
-    heapSort(array);
-    logic_div.innerHTML = "The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from left to right. Each time we will find the minimum element of the unsorted array and swap it with the first element of the unsorted array. This way the element will be inserted at it's correct position and the whole array will be sorted like this"
+  //   sum_delay = 0
+  //   heapSort(array);
+  //   var s = "LOGIC: The array will be divided into 2 parts, unsorted array and sorted array. Sorted array will be formed from left to right. Each time we will find the minimum element of the unsorted array and swap it with the first element of the unsorted array. This way the element will be inserted at it's correct position and the whole array will be sorted like this"
+  //   updateAlgoLogic(s);
   }
   const QuickSortClick = () => {
-    sum_delay = 0
-    quickSort(array);
-    logic_div.innerHTML = ""
+  //   sum_delay = 0
+  //   quickSort(array);
+  //   var s = "LOGIC: "
+  //   updateAlgoLogic(s);
   }
-  // const MergeSortClick = () => {
-  //   const animations = mergeSort(array)
-  //   // compareStart
-  //   // compareEnd
-  //   // override
-  //   const arrayBars = document.getElementsByClassName("app__array__container__bar")
-  //   for( let i = 0 ; i < animations.length ; i++ ){ // yaha pr if I was using a for each loop the it was giving 'undefined' somehow
-  //     const animation = animations[i]
-  //     const [compareIdx1, compareIdx2] = animation.compareStart
-  //     const [idx, newHeight] = animation.override;
-  //     const barOneStyle = arrayBars[compareIdx1].style
-  //     const barTwoStyle = arrayBars[compareIdx2].style;
-  //     for(let j = 0 ; j < 3 ; j++){
-  //       if(j===0){
-  //         setTimeout(() => {
-  //           barOneStyle.backgroundColor = 'red';
-  //           barTwoStyle.backgroundColor = 'red';
-  //         }, i*10)
-  //       }
-  //       else if(j===1){
-  //         setTimeout(() => {
-  //           barOneStyle.backgroundColor = 'white';
-  //           barTwoStyle.backgroundColor = 'white';
-  //         }, i*10)
-  //       }
-  //       else {
-  //         setTimeout(() => {
-  //           const bar = arrayBars[idx].style
-  //           bar.height = `${newHeight}px`;
-  //           bar.backgroundColor = 'yellow';
-  //         }, i*5)
-          
-  //       }
-  //     }
-      
-  //   }
-  // }
   return (
     <div className="app">
       <div className = "app__header">
@@ -186,7 +158,7 @@ function App() {
             </div>
           ))}
       </div>
-      <div className="app__algo__logic">
+      <div id="app__algo__logic">
       </div>
     </div>
     
