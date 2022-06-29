b9e2ef3490a66b19eff4166c7253ec0549d2fa54
Yuvraj Garg <yuvraj.garg101@gmail.com>
2022-06-29 14:08:16 +0530

    edited footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './Footer.css'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 
 
@@ -11,7 +10,6 @@ function Footer() {
       <div className="footer__text">Made with ❤ by Yuvraj Garg</div>
       <div className ='footer__icon'><a href = 'https://github.com/yuvrajgarg' target = 'blank'><GitHubIcon /></a></div> 
       <div className ='footer__icon'><a href='https://www.linkedin.com/in/yuvrajgarg100/' target='_blank'><LinkedInIcon /></a></div>
-      <div className ='footer__icon'><a href='' target='_blank'><InstagramIcon /></a></div>
       <div className ='footer__icon'><a href='https://www.youtube.com/channel/UClEGLDy9TcEnBMX5A94Cikg' target='_blank'><YouTubeIcon /></a></div>
     </div>
 }
